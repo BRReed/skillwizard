e18ec0894edfaab7c00060c95065b61fbee70600
BRReed <b.r.reed@gmail.com>
2023-05-01 21:25:48 -0700

    added basic user input for module selection and separating requested modules into list
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- docker/main.py
+++ docker/main.py
@@ -13,7 +13,7 @@ def welcome_message():
     )
 
 def get_input():
-    pass
+    return input(">")
 
 def get_qa_list():
     return {}
@@ -22,15 +22,39 @@ def get_module_csv_list():
     "Returns list of file names from modules subdir"
     return [file for file in listdir("./modules")]
 
+def print_available_modules(module_list):
+    for i, module in enumerate(module_list):
+        print(i, ". ", module)
+
+def make_input_list(s):
+    l = []
+    for c in s:
+        if not c.isnumeric() and not c.isspace():
+            print("Invalid entry " + c + ", please try again.")
+            return
+        if c.isnumeric():
+            l.append(c)
+    return l
+    
+
 def main():
     welcome_message()
     module_names = get_module_csv_list()
     if not module_names:
         print("Unable to locate modules.")
         exit()
-    print("Please enter the module numbers you want to practice separated by a space.")
-    for i, module in enumerate(module_names):
-        print(i, ". ", module)
+    while True:
+        print("Please enter the module numbers you want to practice separated by a space.")
+        print_available_modules(module_names)
+        user_modules = get_input()
+        selected_modules_list = make_input_list(user_modules)
+        if not selected_modules_list:
+            print("Enter '-1' to exit")
+            continue
+        break
+    print(selected_modules_list)
+    # create module dict, start while loop for program
+    
 
 
 
