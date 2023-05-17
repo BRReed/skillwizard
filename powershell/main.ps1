17a6402af771241f5f4f638b67a259b8f4ef47e8
BRReed <b.r.reed@gmail.com>
2023-05-16 20:50:25 -0700

    added Send-WelcomeMessage func
    Signed-off-by: BRReed <b.r.reed@gmail.com>

---
+++ powershell/main.ps1
@@ -0,0 +1,14 @@
+function Send-WelcomeMessage {
+    Write-Host " "
+    Write-Host " "
+    Write-Host "  ____,__, ,__, __,  __,    _   _,__, ___,  ____,____, ____, "
+    Write-Host " (-(__(-|_/(-| (-|  (-|    (-|  |(-| (- /  (-/_|(-|__)(-|  \ "
+    Write-Host " ,____)_| \,_|_,_|__,_|__,  _|/\|,_|_,_/__,_/  |,_|  \,_|__/ "
+    Write-Host "                       LEARN BY ROTE IN THE TERMINAL"
+    Write-Host " "
+    Write-Host " "
+    Write-Host " "
+    Write-Host " "
+}
+
+Send-WelcomeMessage
